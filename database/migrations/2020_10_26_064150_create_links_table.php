c797fe302d1a2e7af9c42f8d441e224fe5999945
Andrij Kshanovskij <andrewafk2000@gmail.com>
2020-10-26 11:25:16 +0200

    Create tags, events_tags, links migration, update events migration

--- database/migrations/2020_10_26_064150_create_links_table.php
+++ database/migrations/2020_10_26_064150_create_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventsTable extends Migration
+class CreateLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('tags');
-            $table->string('links');
-            $table->integer('category_id');
+            $table->string('content');
+            $table->integer('event_id');
             $table->string('reason')->default('');
             $table->timestamps();
             $table->softDeletes();
@@ -32,6 +30,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('links');
     }
 }
