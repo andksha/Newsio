cbedeb72b394bb142af26bf7d8e30de086698dc5
Andrij Kshanovskij <andrewafk2000@gmail.com>
2020-11-18 11:37:44 +0200

    Remove edit event method from Moderator\EventController

--- app/Http/Controllers/Moderator/EventController.php
+++ app/Http/Controllers/Moderator/EventController.php
@@ -5,40 +5,16 @@ namespace App\Http\Controllers\Moderator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Newsio\Boundary\CategoryBoundary;
 use Newsio\Boundary\IdBoundary;
-use Newsio\Boundary\LinksBoundary;
 use Newsio\Boundary\StringBoundary;
-use Newsio\Boundary\TagsBoundary;
-use Newsio\Boundary\TitleBoundary;
 use Newsio\Contract\ApplicationException;
 use Newsio\UseCase\Moderator\RemoveEventUseCase;
 use Newsio\UseCase\Moderator\RemoveLinkUseCase;
 use Newsio\UseCase\Moderator\RestoreEventUseCase;
 use Newsio\UseCase\Moderator\RestoreLinkUseCase;
-use Newsio\UseCase\EditEventUseCase;
 
 class EventController extends Controller
 {
-    public function edit(Request $request)
-    {
-        $uc = new EditEventUseCase();
-
-        try {
-            $event = $uc->edit(
-                $request->id,
-                new TitleBoundary($request->title),
-                new TagsBoundary($request->tags),
-                new LinksBoundary($request->links),
-                new CategoryBoundary($request->category)
-            );
-        } catch (ApplicationException $e) {
-            return view('error')->with(['message' => $e->getMessage()]);
-        }
-
-        return view('event.events')->with(['event' => $event]);
-    }
-
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
